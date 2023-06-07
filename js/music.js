9aa38e10951794d761fae6d015edbcfb6d6dd1a9
Cannellespeculos <cjeanpierre@edenschool.fr>
2023-06-07 09:33:37 +0200

    k

--- js/music.js
+++ js/music.js
@@ -1,11 +1,15 @@
-// function musics(music) {
-//     const notes = ["Do", "Sol", "Fa", "Re", "Mi", "Si", "La"];
-//     let nbrnotes = ""
+function musics(music) {
+    const notes = ["Do", "Sol", "Fa", "Re", "Mi", "Si", "La"];
+    let nbrnotes = []
 
-//     for (let i = 0; i < music.length; i++) {
-//         if (music[i] === "") {
+    for (let i = 0; i < music.length; i++) {
+        switch (music[i]) {
+            case value:
 
-//         }
+                break;
 
-//     }
-// }
+            default:
+                break;
+        }
+    }
+}
